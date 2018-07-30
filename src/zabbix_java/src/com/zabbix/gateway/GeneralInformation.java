7037bc674c733f5f30a98a9b2faaf6171f405efb
Fernando Brunelli Costa <fernando@casetecnologia.com.br>
2018-07-30 00:00:00 -0300

    Release 3.4.12
    Importing release 3.4.12

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,9 +22,9 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "25 June 2018";
-	static final String REVISION = "82160";
-	static final String VERSION = "3.4.11";
+	static final String REVISION_DATE = "30 July 2018";
+	static final String REVISION = "83229";
+	static final String VERSION = "3.4.12";
 
 	static void printVersion()
 	{
