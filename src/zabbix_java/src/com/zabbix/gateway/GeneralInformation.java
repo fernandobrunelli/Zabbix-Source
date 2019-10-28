26c2412e9d1162d291a55ad517bf7052ec8a36b4
Fernando Brunelli Costa <1944902+fernandobrunelli@users.noreply.github.com>
2019-10-28 00:00:00 -0300

    Release 4.0.14
    Importing release 4.0.14

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,9 +22,9 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "2 October 2019";
-	static final String REVISION = "4e383bb6c5";
-	static final String VERSION = "4.0.13";
+	static final String REVISION_DATE = "28 October 2019";
+	static final String REVISION = "880528396b";
+	static final String VERSION = "4.0.14";
 
 	static void printVersion()
 	{
